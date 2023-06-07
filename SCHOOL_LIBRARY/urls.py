7d5be6ffc320c6d13f5ba32037bedb967e43d1bf
Bill <131465945+RgixSys@users.noreply.github.com>
2023-06-07 16:21:53 +0200

    safi

--- SCHOOL_LIBRARY/urls.py
+++ SCHOOL_LIBRARY/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     3. juste un test pour gittftyffyufyufyufduduy
+    4. jewe iwanje vyakunze
+    
 """
 from django.contrib import admin
 from django.urls import path,include
