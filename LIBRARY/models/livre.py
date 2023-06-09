81d91c3dcd476d2780b9b76b6be176e2eedc9187
Bill <131465945+RgixSys@users.noreply.github.com>
2023-06-09 13:21:30 +0200

    modification de la table livre
    fait par Israel

--- LIBRARY/models/livre.py
+++ LIBRARY/models/livre.py
@@ -1,1 +1,20 @@
-"""pour les livres"""
+"""pour les livres modeling"""
+from django.db import models
+class livre(models.Model):
+    catchchoice = [
+        ('histoire','histoire'),
+        ('geographie','geographie'),
+        ('histoire & geographie','histoire & geographie'),
+        ('informatique','informatique'),
+        ('busness','busness')
+    ]
+    id_livre = models.CharField(max_length=30)
+    id_auteur = models.CharField(max_length=30)
+    titre = models.CharField(max_length=100)
+    exemplaire = models.IntegerField
+    annee_publication = models.DateField
+    categorie = models.CharField(max_length=100,choices=catchchoice,default='histoire')
+    
+    def __str__(self):
+        return self.titre
+    
