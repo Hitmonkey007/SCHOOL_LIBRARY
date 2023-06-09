4b8e4d79726f1d1b25e537b2cb1e53d87e482b78
NestaDV <131465954+NestaDV@users.noreply.github.com>
2023-06-09 16:16:28 +0200

    commit pour les lecteurs

--- LIBRARY/models.py
+++ LIBRARY/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class livre(models.livre):
+    id_livre = models.CharField(max_length=30)
+    id_auteur = models.CharField(max_length=30)
+    titre = models.CharField(max_length=100)
+    exemplaire = models.IntegerField
+    annee_publication = models.DateField
