d7b9e782ccb7de049fdd5f57ddf7c961ef10b2c8
eberto igiraneza <132572339+ebertigiraneza@users.noreply.github.com>
2023-06-12 10:56:00 +0200

    modification de lecteur.py

--- LIBRARY/models/lecteur.py
+++ LIBRARY/models/lecteur.py
@@ -1,1 +1,18 @@
 """pour les lecteurs"""
+
+from django.db import models
+
+
+class Lecteur(models.Model):
+    catchoice= [
+        ('homme', 'Homme'),
+        ('femme', 'Femme')
+        ]
+    nom= models.CharField(max_length=40)
+    prenom = models.CharField(max_length=40)
+    Adresse = models.CharField(max_length=40)
+    categorie=models.CharField(max_length=30,choices=catchoice,default='homme')
+    telephone=models.PositiveIntegerField()
+    #utiliser pour donner un livre
+    def __str__(self):
+        return str (self.nom)+'['+str(self.prenom)+']'
