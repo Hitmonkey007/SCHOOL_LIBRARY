f6871abd71c1b60e1a48c0c67e97163f5dae2c8e
eberto igiraneza <132572339+ebertigiraneza@users.noreply.github.com>
2023-06-13 20:41:56 +0200

    update urls.py

--- LIBRARY/urls.py
+++ LIBRARY/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', home.index, name='home'),
     path('contact', home.contact, name='contact'),
+    path('ajout_lecteur', lecteure.ajout_lecteur, name='ajout_lecteur'),
+    path('lecteurs', lecteure.lecteurs, name='lecteurs')
 ]
