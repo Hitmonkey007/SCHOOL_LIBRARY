c9a8f42809cb8fbd2571bf234339d76aac9433e0
eberto igiraneza <132572339+ebertigiraneza@users.noreply.github.com>
2023-06-16 02:07:21 +0200

    Update lecteure.py

--- LIBRARY/views/lecteure.py
+++ LIBRARY/views/lecteure.py
@@ -1,14 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpRequest
+from django.contrib import messages
 
-def ajout_lecteur(request):
+from LIBRARY.models import Lecteur
+from LIBRARY.Forms import LecteurForm
+
+def add(request):
     return render(
         request,
-        'app/home/Ajout_lecteur.html'
+        'app/lecteurs/Add.html'
     )    
 
-def lecteurs(request):
+def index(request):
+     assert isinstance(request, HttpRequest)
+     orders = Lecteur.objects.all()
+     return render(
+        request,
+        'app/lecteurs/index.html',
+        {
+            'lecteurs': Lecteur
+        }
+    )
+
+def edit(request):
+    assert isinstance(request, HttpRequest)
+    orders = Lecteur.objects.all()
     return render(
         request,
-        'app/home/lecteurs.html'
-    )    
+        'app/lecteurs/edit.html'
+    )        
+
+def delete(request, id):
+    lecteur= Lecteur.objects.get(pk=id)
+    lecteur.delete()
+    return redirect('/lecteurs')
+
+def update(request, id):
+    if request.method == 'POST':
+        if id == 0 :
+            form = LecteurForm(request.POST)
+        else :
+            lecteur = Lecteur.objects.get(pk=id) 
+            form = LecteurForm(request.POST,instance=lecteur)
+        if form.is_valid() :
+            form.save()
+    return redirect('/lecteurs')        
