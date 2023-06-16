4dc4df5547cec1dc8a882a72217e7ddacd38a53f
eberto igiraneza <132572339+ebertigiraneza@users.noreply.github.com>
2023-06-16 02:06:57 +0200

    Update urls.py

--- LIBRARY/urls.py
+++ LIBRARY/urls.py
@@ -1,3 +1,4 @@
+from LIBRARY.views.lecteure import delete
 from django.contrib import admin
 from django.urls import path
 from LIBRARY.views import *
@@ -7,7 +8,10 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', home.index, name='home'),
     path('contact', home.contact, name='contact'),
-    path('ajout_lecteur', lecteure.ajout_lecteur, name='ajout_lecteur'),
-    path('lecteurs', lecteure.lecteurs, name='lecteurs'),
+    path('ajout_lecteur', lecteure.add, name='lecteur_add'),
+    path('lecteurs', lecteure.index, name='lecteur_index'),
     path('emprunts/', emprunts.index, name='emprunt_index'),
+    path('lecteur_edit', lecteure.edit, name='lecteur_edit'),
+    path('lecteur_delete', lecteure.delete, name='lecteur_delete'),
+    path('lecteur_update', lecteure.update, name='lecteur_update'),
 ]
