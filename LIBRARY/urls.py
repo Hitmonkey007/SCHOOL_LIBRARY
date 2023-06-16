046de84b29e329b9b995498ba6520fa1cdb265cb
Hitmonkey007 <97385201+Hitmonkey007@users.noreply.github.com>
2023-06-16 11:01:33 +0200

    New paths (signup, signin)

--- LIBRARY/urls.py
+++ LIBRARY/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from LIBRARY.views import *
-
+from . import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', home.index, name='home'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
     path('contact', home.contact, name='contact'),
     path('ajout_lecteur', lecteure.ajout_lecteur, name='ajout_lecteur'),
     path('lecteurs', lecteure.lecteurs, name='lecteurs'),
