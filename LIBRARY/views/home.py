525ca007326eda66f71b64af1744f6ad6c085d19
Hitmonkey007 <97385201+Hitmonkey007@users.noreply.github.com>
2023-06-16 11:49:36 +0200

    Signup def configured

--- LIBRARY/views/home.py
+++ LIBRARY/views/home.py
@@ -12,6 +12,22 @@ def index(request):
     )
  
 def signup(request):
+    if request.method == "POST":
+    
+        uname = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['Email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+       
+
+        myuser = User.objects.create_user(uname, email, pass1)
+        myuser.save()
+        messages.success(request, 'Your account has been successfully created')
+        return redirect('signin')
+    
     
     return render(request, 'app/home/signup.html')
 
