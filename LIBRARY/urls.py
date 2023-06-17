a59fc3ca1c9af7b5a8c6d527b4b57be5f275225d
eberto igiraneza <132572339+ebertigiraneza@users.noreply.github.com>
2023-06-17 08:30:56 +0200

    Update urls.py

--- LIBRARY/urls.py
+++ LIBRARY/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('ajout_lecteur/', lecteure.add, name='lecteur_add'),
     path('lecteurs/', lecteure.index, name='lecteur_index'),
     path('emprunts/', emprunts.index, name='emprunt_index'),
-    path('lecteur_edit/', lecteure.edit, name='lecteur_edit'),
-    path('lecteur_delet/', lecteure.delete, name='lecteur_delete'),
-    path('lecteur_updat/', lecteure.update, name='lecteur_update'),
+    path('lecteur_edit/<id>', lecteure.edit, name='lecteur_edit'),
+    path('lecteur_delete/<id>', lecteure.delete, name='lecteur_delete'),
+    path('lecteur_updat/<id>', lecteure.update, name='lecteur_update'),
     path('emprunts/index', emprunts.index, name='emprunt_index'),
     path('emprunts/ajout_emprunt',emprunts.ajout_emprunt, name='emprunts_ajout')
 ]
