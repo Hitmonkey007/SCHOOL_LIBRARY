efac2cfb9a32a086ddeb8537d08a7518b4006f66
NestaDV <131465954+NestaDV@users.noreply.github.com>
2023-06-20 21:27:55 +0200

    la modification de l'urls

--- LIBRARY/urls.py
+++ LIBRARY/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', sigmup.signup, name='signup'),
     path('sigmup/', sigmup.signin, name='signin'),
-    path('home/', home.index, name='home_index'),
+    path('home/', home.index, name='home'),
     path('contact', home.contact, name='contact'),
     path('ajout_lecteur/', lecteure.add, name='lecteur_add'),
     path('lecteurs/', lecteure.index, name='lecteur_index'),
