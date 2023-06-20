18d8f377482c9d713ad0287512a2461a41d59015
Theoneste <marwanditheoneste01@gmail.com>
2023-06-20 07:23:04 +0200

    import messages

--- LIBRARY/views/emprunts.py
+++ LIBRARY/views/emprunts.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import redirect, render
+from django.contrib import messages
 def index(request):
     assert isinstance(request, HttpRequest)
     emprunts = Emprunt.objects.all()
