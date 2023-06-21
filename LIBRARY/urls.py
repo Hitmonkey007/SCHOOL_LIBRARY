50d7a1581618c4546dbe8972e73a6b2a9b08d371
Marwandi <marwanditheoneste01@gmail.com>
2023-06-21 11:57:52 +0200

    add urls in path for emprunt

--- LIBRARY/urls.py
+++ LIBRARY/urls.py
@@ -12,18 +12,19 @@ urlpatterns = [
     path('contact', home.contact, name='contact'),
     path('ajout_lecteur/', lecteure.add, name='lecteur_add'),
     path('lecteurs/', lecteure.index, name='lecteur_index'),
-    path('emprunts/', emprunts.index, name='emprunt_index'),
+    
     path('lecteur_edit/<id>', lecteure.edit, name='lecteur_edit'),
     path('lecteur_delete/<id>', lecteure.delete, name='lecteur_delete'),
     path('lecteur_updat/<id>', lecteure.update, name='lecteur_update'),
-    path('emprunts/index', emprunts.index, name='emprunt_index'),
-    path('emprunts/ajout_emprunt',emprunts.ajout_emprunt, name='emprunts_ajout'),
-    path('emprunts/ajout_emprunt',emprunts.ajout_emprunt, name='emprunts_ajout'),
+   
+  
+   
     path('livre/ajouter_livre',livre.ajouter_livre, name='livre_ajout') ,
 
     path('emprunts/', emprunts.index, name='emprunt_index'),
     path('emprunts/index', emprunts.index, name='emprunt_index'),
     path('emprunts/ajout_emprunt',emprunts.ajout_emprunt, name='emprunts_ajout'),
+   
 
 
     
